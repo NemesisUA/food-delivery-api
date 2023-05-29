51666dc7063fe6c0629cc7b32c4d2e8bca43e393
NemesisUA <display.n0ne.01.gmail.com>
2023-05-29 09:40:27 +0300

    add more headers for POSR requests

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.json());
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
