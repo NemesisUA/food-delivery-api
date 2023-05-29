9fcea726e6dd1defecd2c08179b4b62c7c137f0d
NemesisUA <display.n0ne.01.gmail.com>
2023-05-29 09:23:42 +0300

    add headers for CORS

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-})
+});
 
 app.use('/api/products', productRoutes);
 
